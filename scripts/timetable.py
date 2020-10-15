29da4281aeea131349a5dc64646815c4aa6797a1
Vishal Vignesh <vishalvignesh.15@gmail.com>
2020-10-15 16:56:05 +0530

    Completed timetable.py
    Fully implemented timetable.py

--- scripts/timetable.py
+++ scripts/timetable.py
@@ -1,57 +1,50 @@
+from os import path
+from os.path import isfile
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter
 import pickle
+import os.path
 
 times=['08.45 am','09.45 am','11.00 am','12.00 pm','01.00 pm','02.00 pm','03.15 pm','04.15 pm']
-monday=[]
-tuesday=[]
-wednesday=[]
-thursday=[]
-friday=[]
-saturday=[]
-classes=[]
-def inputTimeTable(day):
-    char='y'
-    while(char == 'y'):
-        coursecode = input("Enter the course code ")
-        timing = prompt("Enter the time ",completer=WordCompleter(times))
-        temp=(coursecode,timing)
-        classes.append(temp)
-        char = input("Do you want to input another course?(y/n) ")
+days = ['monday','tuesday','wednesday','thursday','friday','saturday']
 
 if __name__ == "__main__":
-    # initializing days
-    try:
-        print("Enter the courses for Monday ")
-        inputTimeTable("monday")
-        print("Enter the courses for Tuesday ")
-        inputTimeTable("tuesday")
-        print("Enter the courses for Wednesday ")
-        inputTimeTable("wednesday")
-        print("Enter the courses for Thursday ")
-        inputTimeTable("thursday")
-        print("Enter the courses for Friday ")
-        inputTimeTable("friday")
-        print("Enter the courses for Saturday ")
-        inputTimeTable("saturday")
-    except:
-        print("Error Occur")
+    courses = []
+    if os.path.isfile(os.getcwd()+'\\timetables\\'+'courses'+'.pkl') is True:
+        inp_course = open(os.getcwd()+'\\timetables\\'+'courses'+'.pkl',"rb")
+        courses = pickle.load(inp_course)
+        inp_course.close()
 
-    # saving timetable
-    print("Attempting to save the timetable... ")
-    try:
-        with open("../timetables/monday.pkl","w") as md:
-            pickle.dump(monday,md)
-        with open("../timetables/tuesday.pkl","w") as td:
-            pickle.dump(tuesday,td)
-        with open("../timetables/wednesday.pkl","w") as wd:
-            pickle.dump(wednesday,wd)
-        with open("../timetables/thursday.pkl","w") as thd:
-            pickle.dump(thursday,thd)
-        with open("../timetables/friday.pkl","w") as fd:
-            pickle.dump(friday,fd)
-        with open("../timetables/saturday.pkl","w") as sd:
-            pickle.dump(saturday,sd)
-        print("Timetable Saved Succesfully! ")
-    except:
-        print("Failed to save the timetable! Check Permissions ")
+    for i in days:
+        dir = os.getcwd()+'\\timetables\\'+i+'.pkl'
+        if os.path.isfile(dir) is False:
+            print('Enter the timetable for '+i)
+            temp = []
+            flag = 'y'
+            while(flag == 'y'):
+                cc = None
+                time = None
+                try:
+                    cc = prompt('Enter the course code: ',completer=WordCompleter(courses))
+                    if cc not in courses:
+                        courses.append(cc)
+                    time = prompt('Enter the time: ',completer=WordCompleter(times))
+                except:
+                    print('Error occured')
+                temp.append((cc,time))
+                flag = input('Do you want to add another course? (y/n)')
+            #attempting to save
+            print('Saving',i,'\b','s timetable')
+            try:
+                file_object = open(dir,'wb')
+                pickle.dump(temp,file_object)
+                file_object.close()
+                print('Successfully saved '+i)
+            except:
+                print('Failed to save')
+    dirc = os.getcwd()+'\\timetables\\'+'courses'+'.pkl'
+    if os.path.isfile(dirc) is False:
+        file_object = open(dirc,'wb')
+        pickle.dump(courses,file_object)
+        file_object.close()
+
