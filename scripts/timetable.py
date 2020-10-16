db4eef194106ae7e2019e76889ef2c9aaeb72f6f
Vishal Vignesh <vishalvignesh.15@gmail.com>
2020-10-17 04:37:56 +0530

    Update timetable.py
    timetable.py now incomplete, @RamNarayan27 @snr200 please complete it if possible

--- scripts/timetable.py
+++ scripts/timetable.py
@@ -5,8 +5,8 @@ from prompt_toolkit.completion import WordCompleter
 import pickle
 import os.path
 
-times=['08.45 am','09.45 am','11.00 am','12.00 pm','01.00 pm','02.00 pm','03.15 pm','04.15 pm']
-days = ['monday','tuesday','wednesday','thursday','friday','saturday']
+times=['08:45 AM','09:45 AM','11:00 AM','12:00 PM','01:00 PM','02:00 PM','03:15 PM','04:15 PM']
+days = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
 
 if __name__ == "__main__":
     courses = []
@@ -19,6 +19,7 @@ if __name__ == "__main__":
         dir = os.getcwd()+'\\timetables\\'+i+'.pkl'
         if os.path.isfile(dir) is False:
             print('Enter the timetable for '+i)
+            print('If there are no courses for the day, type nill as course code')
             temp = []
             flag = 'y'
             while(flag == 'y'):
@@ -26,6 +27,9 @@ if __name__ == "__main__":
                 time = None
                 try:
                     cc = prompt('Enter the course code: ',completer=WordCompleter(courses))
+                    if cc == 'nill':
+                        temp = []
+                        break
                     if cc not in courses:
                         courses.append(cc)
                     time = prompt('Enter the time: ',completer=WordCompleter(times))
@@ -33,6 +37,14 @@ if __name__ == "__main__":
                     print('Error occured')
                 temp.append((cc,time))
                 flag = input('Do you want to add another course? (y/n)')
+            ############################################################################################################
+            #                ADD A CODE HERE THAT FORMATS THE LIST TEMP, IN ORDER OF THE CLASSES,                      #
+            #     THE LIST WILL HAVE TUPLES IN IT IN THE DESIGN OF [('cse308', '08.45 am'), ('cse301', '11.00 am')]    #
+            #                       COMPARE THE TIMES OF EACH TUPLE[1] AND SORT THEM                                   #
+            #       NOTE THE AM AND PM, SO SORT THEM SEPARATELY, MAINTAINGING THEIR OWN ORDER OF FIRST AM THEN PM      #
+            #              ALSO NOTE THIS FUNCTION HAS TO BE IN THE FOR LOOP - OUTSIDE THE WHILE LOOP                  #
+            ############################################################################################################
+
             #attempting to save
             print('Saving',i,'\b'+'s timetable')
             try:
