1a44b13e9d0c7ef17d6a9a7556869ee84264f633
Vishal Vignesh <vishalvignesh.15@gmail.com>
2020-10-18 21:43:04 +0530

    Update webPageHandler.py
    Basic implementation of webpagehandler, able to create the profile, load it and open a webpage

--- scripts/webPageHandler.py
+++ scripts/webPageHandler.py
@@ -5,7 +5,39 @@
 # join the link
 # close the meet when the number of participants fall below 10
 
+#from scripts.usernotifier import sendNotif
 import selenium 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+import os
 
-def web_page_opener(link): #the function receives the meet link, the function first needs perform the above mentioned functions and return nothing
-    print('test')
+def profile_creator(link): #the function receives the meet link, the function first needs perform the above mentioned functions and return nothing
+    drivers = os.getcwd().split('\\scripts')[0]+'\\web_drivers'
+    #meetdriver = None
+    temp = 'user-data-dir='+drivers+'\myprofile'
+    to = Options()
+    to.add_argument(temp)
+    meetdriver = webdriver.Chrome(executable_path=drivers+'\chromedriver.exe', chrome_options=to)
+    meetdriver.get('http://www.google.com')
+    while(  ):#condition to keep the browser open
+        #keep running the code / browser open 
+        #probably the participants part
+        
+    #sendNotif(5)
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+profile_creator ('mail.google.com')
