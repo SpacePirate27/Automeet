edad19fbc2dac14131b83bef4b42bd79fb1fa2b3
Vishal Vignesh <vishalvignesh.15@gmail.com>
2020-10-18 21:42:30 +0530

    Update usernotifier.py
    Removed iconpath, need to verify the functionality of code

--- scripts/usernotifier.py
+++ scripts/usernotifier.py
@@ -12,12 +12,12 @@ notif_desc = ["Entered the class successfully",
         "Exited the class successfully",
         "Loss of internet",
         "Found the class link",
-        "Unable to find meeting link"]
+        "Unable to find meeting link",
+        'login with your sastra email id when the browser opens then close it']
 def sendNotif(param):
     Notification(
 	title=notif_title,
     description=notif_desc[param],
-	icon_path='Paomedia-Small-N-Flat-Bell.ico',
 	duration=5,
 	urgency=Notification.URGENCY_CRITICAL
 ).send()
