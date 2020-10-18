f1ade242d3b3447f9ddc72f9b7aa2183b3ee6b2d
Vishal Vignesh <vishalvignesh.15@gmail.com>
2020-10-18 06:24:15 +0530

    Update linkScraper.py
    Linkscrapper now checks the received mail, filters the best possible mail with a step-by-step approach and obtains the meet link from the mail

--- scripts/linkScraper.py
+++ scripts/linkScraper.py
@@ -2,3 +2,47 @@
 # the mail will be in a html rendering
 # grep for classroom.google.com and retirieve the entire link
 # call webPageHandler
+
+
+from scripts.mailaccess import get_the_link
+
+def linkscraper_runner(mailist,classinfo):
+    classcode = classinfo[0][0]
+    classtime = classinfo[0][1]
+
+    #the program follows a step by step progressive manner of checking if the retrieved mails are possibly the mail with the link for the correct google meet  
+
+    final_body = ''
+    first_value = 3
+    first_filter = [] #this filter simply checks if there is a google meet link in the obtained mails
+    while len(first_filter) == 0:
+        for i in mailist:
+            if 'meet.google.com' in i.split('/'):
+                first_filter.append(i)
+        if len(first_filter) == 0: #if the acquired mails has no meet.google.com links, get more
+            mailist = get_the_link(classinfo,first_value + 3,rerunner=True)
+            first_value += 3
+        
+    second_filter = [] #this filter tries to check if the subject code or the time of the class is mentioned
+    for i in first_filter:
+        if classtime in i.split(' '):
+            second_filter.append(i)
+        if classcode in i.split(' ') and i not in second_filter:
+            second_filter.append(i)
+        
+    if len(second_filter) != 0: #if no link passes the second filter, then take the newest first filter pass, or take the first second filter if any links passes the second filter
+        final_body = second_filter[0]
+    else:
+        final_body = first_filter[0]
+
+    final_body = final_body.replace('\r',' ')
+    final_body = final_body.replace('\n',' ')
+
+    final_list = final_body.split(' ') #retrieve the link from the body of the mail and return it back to the main function
+
+    link = ''
+    for i in final_list:
+        if 'meet.google.com' in i.split('/'):
+            link = i
+    
+    return link
