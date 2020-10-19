20cf45e3e2b0d73259d8feddabd7fad8b797505e
Vishal Vignesh <vishalvignesh.15@gmail.com>
2020-10-19 14:38:40 +0530

    Update webPageHandler.py
    Added extra condition to disconnect from google meet

--- scripts/webPageHandler.py
+++ scripts/webPageHandler.py
@@ -11,6 +11,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 import os
+import datetime
 
 def web_page_opener(link):
     drivers = os.getcwd().split('\\scripts')[0]+'\\web_drivers'
@@ -18,6 +19,9 @@ def web_page_opener(link):
     to = Options()
     to.add_argument(temp)
     to.add_argument("--mute-audio")
+
+    time1 = datetime.datetime.now()
+
     meetdriver = webdriver.Chrome(executable_path=drivers+'\chromedriver.exe', chrome_options=to)
     meetdriver.get(link)
 
@@ -31,18 +35,26 @@ def web_page_opener(link):
     join_btn = meetdriver.find_element_by_xpath('/html/body/div[1]/c-wiz/div/div/div[5]/div[3]/div/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div[1]').click()
 
     time.sleep(10)
-
+    
     people_btn = meetdriver.find_element_by_xpath('/html/body/div[1]/c-wiz/div[1]/div/div[5]/div[3]/div[6]/div[3]/div/div[2]/div[1]').click()
 
     time.sleep(5)
+
+
     nos = 20
     while nos > 15:
         time.sleep(15)
-        nos = meetdriver.find_element_by_xpath('/html/body/div[1]/c-wiz/div[1]/div/div[5]/div[3]/div[3]/div/div[2]/div[2]/div[1]/div[1]').text        
-        nos = nos.replace('(','')
-        nos = nos.replace(')','')
-        nos = nos.replace('People','')
-        nos = nos.replace('\n','')
-        nos = int(nos)
-    
+        try:
+            nos = meetdriver.find_element_by_xpath('/html/body/div[1]/c-wiz/div[1]/div/div[5]/div[3]/div[3]/div/div[2]/div[2]/div[1]/div[1]').text
+            nos = nos.replace('(','')
+            nos = nos.replace(')','')
+            nos = nos.replace('People','')
+            nos = nos.replace('\n','')
+            nos = int(nos)
+        except:
+            time2 = datetime.datetime.now()
+            diff = time2 - time1
+            if diff.total_seconds() >= 3600:
+                break
+
     meetdriver.close()
