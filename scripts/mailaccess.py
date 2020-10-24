81d32ad591f8ed2ce01c380f2553c02eee038940
Vishal Vignesh <vishalvignesh.15@gmail.com>
2020-10-24 19:59:00 +0530

    Temporary Changes
    Reworking mailaccess to work with google calendar and google classroom

--- scripts/mailaccess.py
+++ scripts/mailaccess.py
@@ -7,50 +7,5 @@ import pickle
 import scripts.linkscraper as lis
 
 def get_the_link(nextclass,no_of_mails,rerunner=False):
-
-    dirc = os.getcwd()+'\\user_mail_creds\\'+'user_name'+'.pkl'
-
-    usrname = open(os.getcwd()+'\\user_mail_creds\\'+'user_name'+'.pkl',"rb")
-    username = pickle.load(usrname)
-    passw = open(os.getcwd()+'\\user_mail_creds\\'+'password'+'.pkl',"rb")
-    password = pickle.load(passw)
-    usrname.close()
-    passw.close()
-
-    imap = imaplib.IMAP4_SSL("imap.gmail.com")
-    imap.login(username, password)
-    status, messages = imap.select("INBOX")
-    messages = int(messages[0])
-    N = no_of_mails
-
-    list_of_mail = []
-
-    for i in range(messages, messages-N, -1):
-        res, msg = imap.fetch(str(i), "(RFC822)")
-        body = ''
-        for response in msg:
-            if isinstance(response, tuple):
-                msg = email.message_from_bytes(response[1])
-                if msg.is_multipart():
-                    for part in msg.walk():
-                        content_type = part.get_content_type()
-                        content_disposition = str(part.get("Content-Disposition"))
-                        try:
-                            body = part.get_payload(decode=True).decode()
-                        except:
-                            pass
-                        if content_type == "text/plain":
-                            list_of_mail.append(body)
-                else:
-                    content_type = msg.get_content_type()
-                    body = msg.get_payload(decode=True).decode()
-                    if content_type == "text/plain":
-                        list_of_mail.append(body)
-    imap.close()
-    imap.logout()
-
-    if rerunner == False:
-        final_link = lis.linkscraper_runner(list_of_mail,nextclass)
-        return final_link
-    else:
-        return list_of_mail
+    print('Need to change to work with google api')
+    return 'link of google meet'
