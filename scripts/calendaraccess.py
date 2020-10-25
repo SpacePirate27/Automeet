f6a1889f1e2f7c1fc8316a29b57e82380856d4ac
Vishal Vignesh <vishalvignesh.15@gmail.com>
2020-10-25 06:16:22 +0530

    Rework
    The program will now check for links on google calendar or google classroom instead of email

---
+++ scripts/calendaraccess.py
@@ -0,0 +1,42 @@
+import imaplib
+import email
+from email.header import decode_header
+import webbrowser
+import os
+import pickle
+import scripts.linkScraper as lis
+import datetime
+
+from googleapiclient.discovery import build
+
+def get_the_day(gap):
+    days = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
+    day = datetime.date.today() + datetime.timedelta(days=gap)
+    day = days[day.weekday()]
+    return day
+
+def getthelink(nextclass,creds):
+    classcode = nextclass[0][0]
+    classtime = nextclass[0][1]
+    classday = get_the_day(nextclass[1])
+
+    starter = datetime.datetime.now()
+    starttime = datetime.datetime.now().isoformat() + 'Z'
+    endtime = starter + datetime.timedelta(hours=1,minutes=20)
+    endtime = endtime.isoformat() + 'Z'
+    calservice = build('calendar', 'v3', credentials=creds)
+    id_list = []
+    calendar_list = calservice.calendarList().list().execute()
+    for calendar_list_entry in calendar_list['items']:
+        id_list.append(calendar_list_entry['id'])
+    link = None
+
+    for eachid in id_list:
+        eventc = calservice.events().list(calendarId=eachid,timeMin=starttime,timeMax=endtime).execute()
+        eventc = eventc['items']
+        try:
+            link = eventc[0]['conferenceData']['entryPoints'][0]['uri']
+        except:
+            None
+
+    return link
--- scripts/classroomaccess.py
+++ scripts/classroomaccess.py
@@ -1,6 +1,7 @@
 import imaplib
 import email
 from email.header import decode_header
+from os import name
 import webbrowser
 import os
 import pickle
@@ -16,7 +17,7 @@ def get_the_day(gap):
     day = days[day.weekday()]
     return day
 
-def get_the_link(nextclass,creds,rerunner=False):
+def get_the_link(nextclass,creds):
     classcode = nextclass[0][0]
     classtime = nextclass[0][1]
     classday = get_the_day(nextclass[1])
@@ -26,37 +27,41 @@ def get_the_link(nextclass,creds,rerunner=False):
         clcourse = open(os.getcwd()+'\\timetables\\'+'course_classroom'+'.pkl',"rb")
         courses = pickle.load(clcourse)
         clcourse.close()
+    #print(courses)
     
     classroom_name = courses[classcode]
-    
     
     service = build('classroom', 'v1', credentials=creds)
     results = service.courses().list(pageSize=10).execute()
     courses = results.get('courses', [])
     current_course = None
-
-    if not courses:
-        print('No courses found.')
-    else:
-        for course in courses:
-            if course['name'] == classroom_name:
-                current_course = course
+    for course in courses:
+        if course['name'] == classroom_name:
+            current_course = course
+            #print(course)
+        else:
+            #print('Course name is: ',course['name'],' AND CLASSROOM NAME IS: ',classroom_name)
+            None
+    
+    #print('\n\n',current_course['id'],'\n\n')
 
     temp = service.courses().announcements().list(courseId=current_course['id']).execute()
-    latest_annon = temp['announcements'][1]
-    text = latest_annon['text']
-    link = None
-
-    try:
-        link = latest_annon['materials'][0]['link']['url']
-    except:
-        text = text.replace('\r',' ')
-        text = text.replace('\n',' ')
-        textlist = text.split(' ')
-        print(textlist)
-        for i in textlist:
-            if 'meet.google.com' in i.split('/'):
-                link = i
-    
-    print(link)
-    return link
+    starter = 0
+    while starter < 3:
+        latest_annon = temp['announcements'][starter]
+        text = latest_annon['text']
+        link = None
+        try:
+            link = latest_annon['materials'][0]['link']['url']
+        except:
+            text = text.replace('\r',' ')
+            text = text.replace('\n',' ')
+            textlist = text.split(' ')
+            #print(textlist)
+            for i in textlist:
+                if 'meet.google.com' in i.split('/'):
+                    link = i
+        if link == None:
+            starter += 1
+        else:
+            return link
