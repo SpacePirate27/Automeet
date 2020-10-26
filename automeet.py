288d24cccd2fe22efe5c0d0a1cda4c4ae139ad5f
Vishal Vignesh <vishalvignesh.15@gmail.com>
2020-10-26 16:58:39 +0530

    Update automeet.py
    Added a logger to the main file

--- automeet.py
+++ automeet.py
@@ -1,4 +1,11 @@
 from scripts.main import mainrunner
+import traceback
+
 
 if __name__ == "__main__":
-    mainrunner()
+    with open('log.txt','w') as log:
+        try:
+            mainrunner()
+        except:
+            traceback.print_exc(file=log)
+            print('Errors have been written to a log file')
